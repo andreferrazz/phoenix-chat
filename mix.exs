da5321b3cc783aee1d32096b92b450ce0943d6f6
Andre Ferraz <andreferraz25384@gmail.com>
2021-10-24 22:43:22 -0300

    Add `credo` lib to the project.

--- mix.exs
+++ mix.exs
@@ -1,4 +1,6 @@
 defmodule PhoenixChat.MixProject do
+  @moduledoc false
+
   use Mix.Project
 
   def project do
@@ -44,7 +46,8 @@ defmodule PhoenixChat.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
